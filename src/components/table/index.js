9dfd08e02361e62e1b929d592e648145e3469630
Mr-Hamster <44702623+Mr-Hamster@users.noreply.github.com>
2019-05-21 19:48:08 +0300

    Add files via upload

---
+++ src/components/table/index.js
@@ -0,0 +1,67 @@
+import React, {Fragment} from 'react' 
+import {crudBuilder} from '../../services/http'
+import {InputGroup, FormControl, Table} from 'react-bootstrap'
+
+export default class TableN extends React.Component{
+    state={
+        search:'',
+        dataS:[],
+        staticData: [],
+    }
+
+    async componentDidMount(){
+        const response = await crudBuilder('Mr-Hamster/server/data').get()
+        this.setState({dataS: response.data, staticData: response.data})
+    }
+
+    searching = (value) =>{
+        const{staticData} = this.state;
+        if(value === ''){
+            this.setState({dataS: staticData})
+        }
+        else{
+            const str = value.toLowerCase();
+            const dataFiltered = staticData.filter(item => item.name.toLowerCase().includes(str))
+            this.setState({dataS: dataFiltered})  
+        }
+    }
+
+    handleChange = ( {target: {value} }) => {
+        this.setState({
+            search: value,
+        })
+        this.searching(value)
+    }
+
+    render(){
+        return(
+            <Fragment>
+                <InputGroup className="mb-3" style={{width: "50%"}}>
+                    <FormControl aria-describedby="basic-addon1" placeholder='Search by name...' onChange={this.handleChange}/>
+                </InputGroup>
+                <Table striped bordered hover variant="dark" style={{width: "95%"}}>
+                    <thead>
+                        <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        this.state.dataS.map((item, index) => (
+                            <tr key={index}>
+                                <th>{item.id}</th>
+                                <th>{item.name}</th>
+                                <th>{item.price}</th>
+                                <th>{item.quantity}</th>
+                            </tr>
+                        ))
+                    }
+                    </tbody>
+                </Table>
+            </Fragment>
+        );
+    }
+}
---
+++ src/services/http.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+    const serverPrefix = 'http://my-json-server.typicode.com/';
+    
+const getHeaders = () => ({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    accept: 'application/json',
+});
+
+const sendRequest = (url, options) =>
+    axios({
+        ...options,
+        url: serverPrefix + url,
+        headers: getHeaders(),
+    });
+
+export const crudBuilder = url => ({
+    get: () =>sendRequest(url,{method: 'GET'}),
+});
